d99bbb0f79b19fd05fd79421ffef840f13ddc3d7
Yoshietru Nagata <38305549+nagata-yoshiteru@users.noreply.github.com>
2021-02-15 17:34:27 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/kieranb662/CGExtender.git", from: "1.0.1"),
-        .package(url: "https://github.com/kieranb662/Shapes.git", from: "1.0.2"),
+        .package(name: "Shapes", url: "https://github.com/kieranb662/SwiftUI-Shapes.git", from: "1.0.2"),
         .package(url: "https://github.com/kieranb662/bez.git", from: "1.0.0")
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
